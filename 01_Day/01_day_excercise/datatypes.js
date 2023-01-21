6373edfbd1b451489be2a6f1fc464ea159ec1fd8
mfikriam <114622652+mfikriam@users.noreply.github.com>
2023-01-21 17:22:12 +0800

    finished day 1

---
+++ 01_Day/01_day_excercise/datatypes.js
@@ -0,0 +1,6 @@
+// 5. Create datatypes.js file and use the JavaScript typeof operator to check different data types. Check the data type of each variable
+console.log(typeof nama)
+console.log(typeof isMale)
+console.log(typeof biodata)
+console.log(typeof money)
+console.log(typeof 17)
---
+++ 01_Day/01_day_excercise/variable.js
@@ -0,0 +1,5 @@
+// 4. Create a variable.js file and declare variables and assign string, boolean, undefined and null data types
+let nama = 'Muhammad Fikri' // string
+let isMale = true // boolean
+let biodata // undefined
+let money = null // null
