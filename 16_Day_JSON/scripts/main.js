fb169c41222ab4b645a109a4d1e8aedd3e836222
mfikriam <114622652+mfikriam@users.noreply.github.com>
2023-02-06 14:58:45 +0800

    update formatting

--- 16_Day_JSON/scripts/main.js
+++ 16_Day_JSON/scripts/main.js
@@ -5,11 +5,11 @@ const skills = ['HTML', 'CSS', 'JS', 'React', 'Node', 'Python']
 let age = 250;
 let isMarried = true
 const student = {
-	firstName: 'Asabeneh',
-	lastName: 'Yetayehe',
-	age: 250,
-	isMarried: true,
-	skills: ['HTML', 'CSS', 'JS', 'React', 'Node', 'Python',]
+    firstName: 'Asabeneh',
+    lastName: 'Yetayehe',
+    age: 250,
+    isMarried: true,
+    skills: ['HTML', 'CSS', 'JS', 'React', 'Node', 'Python',]
 }
 
 console.log("\n[Number 1]");
@@ -151,10 +151,10 @@ console.log("\n[Number 2]");
 // 2. Find the user who has many skills from the variable stored in *txt*.
 const numSkill = 5
 obj = JSON.parse(txt, (key, value) => {
-	if (key !== "skills" || value.length >= numSkill) {
-		return value
-	}
-	return undefined
+    if (key !== "skills" || value.length >= numSkill) {
+        return value
+    }
+    return undefined
 })
 
 console.table(obj);
